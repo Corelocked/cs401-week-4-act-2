a50034e47b882b478f8659e4088441f8904bc859
Cedric Joshua Palapuz <115864258+Corelocked@users.noreply.github.com>
2025-05-29 13:17:08 +0800

    update migration

--- database/migrations/2025_05_29_043907_create_posts_table.php
+++ database/migrations/2025_05_29_043907_create_posts_table.php
@@ -13,14 +13,14 @@ return new class extends Migration
     {
         Schema::create('posts', function (Blueprint $table) {
             $table->id();
-            $table->string('title');
-            $table->text('content');
+            $table->string('title')->comment('Title of the post');
+            $table->text('content')->comment('Content of the post');
             $table->string('slug');
-            $table->timestamp('publication_date')->nullable();
-            $table->timestamp('last_modified_ate')->nullable();
+            $table->timestamp('publication_date')->comment('publication date')->nullable();
+            $table->timestamp('last_modified_date')->comment('moification date')->nullable();
             $table->string('status')->comment('(D - Draft, P - Published, I - Inactive)');
-            $table->string('featured_image_url');
-            $table->integer('views_count')->default(0);
+            $table->string('featured_image_url')->comment('URL of the featured image');
+            $table->integer('views_count')->comment('view count')->default(0);
         });
     }
 
--- database/migrations/2025_05_29_044007_create_comments_table.php
+++ database/migrations/2025_05_29_044007_create_comments_table.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::create('comments', function (Blueprint $table) {
             $table->id();
             $table->text('comment_content')->comment('Content of the comment');
-            $table->timestammp('comment_date')->comment('Date of the comment');
+            $table->timestamp('comment_date')->comment('Date of the comment');
             $table->string('reviewer_name')->comment('Name of the reviewer');
             $table->string('reviewer_email')->comment('Email of the reviewer');
             $table->boolean('is_hidden')->default(false);  
